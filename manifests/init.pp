a107bb4c1626ed17a1e89f7e8350ee99c16183f8
mh <mh@immerda.ch>
2008-10-02 22:01:14 +0000

    updated module to the new style

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,71 +5,62 @@
 #modules_dir { "ssmtp": }
 
 class ssmtp {
-        case $operatingsystem {
-                centos: { include yum }
-        }
-
-        package { ssmtp:
-                name => $operatingsystem ? {
-                        centos => 'ssmtp',
-                        default => 'ssmtp',
-                },
-                category => $operatingsystem ? {
-                        gentoo => 'mail-mta',
-                        default => '',
-                },
-                ensure => present,
-        }
-
-        #set default mailhub if not yet one is set
-        $mailhub_real = $mailhub ? {
-           '' => 'mail.glei.ch',
-           default => $mailhub
-        }
-        $ssmtp_uses_ssl_real = $ssmtp_uses_ssl ? {
-           '' => 'YES',
-           default => $ssmtp_uses_ssl
-        }
-        $mailhub_port_real = $ssmtp_uses_ssl_real ? {
-           'YES' => ':465',
-           default => ''
-        }
-
-        $ssmtp_hostname_real = $ssmtp_hostname ? {
-            '' => $fqdn,
-            default => $ssmtp_hostname
-        }
-
-        $ssmtp_rewrite_domain_real = $ssmtp_rewrite_domain ? {
-            '' => $fqdn,
-            default => $ssmtp_rewrite_domain
-        }
-
-        $ssmtp_from_line_override_real = $ssmtp_from_line_override ? {
-            '' => 'YES',
-            default => $ssmtp_from_line_override
-        }
-
-        file { ssmtp_config:
-                path => '/etc/ssmtp/ssmtp.conf',
-                owner => root,
-                group => 0,
-                ensure => file,
-                mode => 644,
-                require => Package[ssmtp],
-                content => template('ssmtp/default.erb'),
-        }
-        package { sendmail:
-                name => $operatingsystem ? {
-                        centos => 'sendmail',
-                        default => 'sendmail',
-                },
-                category => $operatingsystem ? {
-                        gentoo => 'mail-mta',
-                        default => '',
-                },
-                ensure => absent,
-                require => Package[ssmtp],
-        }
+    case $operatingsystem {
+        centos: { include ssmtp::centos }
+        gentoo: { include ssmtp::gentoo }
+        default: { include ssmtp::base }
+    }
 }
 
+class sendmail::base {
+    include sendmail::disable
+
+    package { ssmtp:
+        ensure => present,
+    }
+
+    case $mailhub {
+        '': { fail("no \$mailhub defined for ${fqdn}") }
+    }
+
+    $ssmtp_uses_ssl_real = $ssmtp_uses_ssl ? {
+        '' => 'YES',
+        default => $ssmtp_uses_ssl
+    }
+    $mailhub_port_real = $ssmtp_uses_ssl_real ? {
+        'YES' => ':465',
+        default => ''
+    }
+
+    $ssmtp_hostname_real = $ssmtp_hostname ? {
+        '' => $fqdn,
+        default => $ssmtp_hostname
+    }
+
+    $ssmtp_rewrite_domain_real = $ssmtp_rewrite_domain ? {
+        '' => $fqdn,
+        default => $ssmtp_rewrite_domain
+    }
+
+    $ssmtp_from_line_override_real = $ssmtp_from_line_override ? {
+        '' => 'YES',
+        default => $ssmtp_from_line_override
+    }
+
+    file { ssmtp_config:
+        path => '/etc/ssmtp/ssmtp.conf',
+        content => template('ssmtp/default.erb'),
+        require => Package[ssmtp],
+        owner => root, group => 0, mode => 644;
+    }
+}
+
+class ssmtp::centos inherits ssmtp::base {
+    include yum
+}
+
+class ssmtp::gentoo inherits ssmtp::base {
+    Package[ssmtp]{
+        category => 'mail-mta',
+    }
+}
