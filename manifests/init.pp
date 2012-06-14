4b01e21aeb903ad1054918756758409ccc393a85
mh <mh@immerda.ch>
2012-06-14 11:09:17 -0300

    take hiera out of the params

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,13 +2,15 @@
 # Copyright (C) 2007 admin@immerda.ch
 #
 
-class ssmtp {
+class ssmtp(
+  $manage_shorewall = false
+) {
   case $::operatingsystem {
     centos: { include ssmtp::centos }
     gentoo: { include ssmtp::gentoo }
     default: { include ssmtp::base }
   }
-  if hiera('use_shorewall',false) {
+  if $manage_shorewall {
     include shorewall::rules::out::smtp
     include shorewall::rules::smtp::disable
     include shorewall::rules::smtps::disable
